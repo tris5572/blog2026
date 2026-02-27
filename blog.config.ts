02344ccd59f71456a6fb2be2f4124209a807a92d
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-02-28 05:16:46 +0900

    initial commit

---
+++ blog.config.ts
@@ -0,0 +1,8 @@
+export const blogConfig = {
+  title: "Blog 2026",
+  description: "TypeScript 製の自作ブログ",
+  language: "ja",
+  siteUrl: "https://YOUR_GITHUB_USERNAME.github.io",
+  basePath: "/blog2026",
+  author: "YOUR_NAME",
+};
